1a557cd8961c9fd7325cfd5f0f740902b492f20d
Muhammad Naseer <muhammadnaseerkhan1122@gmail.com>
2022-11-19 14:18:03 +0500

    Search done

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Country knowledge',
+      title: 'Country knowledge App',
       theme: ThemeData(
 
         primarySwatch: Colors.purple,
